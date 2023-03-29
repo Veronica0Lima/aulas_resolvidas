a0795d81a9266f6c048afd689a2d9c0c79ff31a2
Verônica Luisa Ribeiro Lima <veronicalrl@al.insper.edu.br>
2023-03-29 16:15:58 -0300

    morte da minha alma

--- 10/src/main/java/br/edu/insper/desagil/aula10/FileManager.java
+++ 10/src/main/java/br/edu/insper/desagil/aula10/FileManager.java
@@ -2,7 +2,9 @@ package br.edu.insper.desagil.aula10;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -15,43 +17,81 @@ public class FileManager {
 	public FileManager() {
 		this.decoder = Charset.forName("UTF-8").newDecoder();
 	}
+	
+	private String readContent(BufferedReader reader) throws IOException {
+	    String content = "";
+	    String line = reader.readLine();
+	    while (line != null) {
+	        content += line;
+	        line = reader.readLine();
+	    }
+	    return content + "\n";
+	}
 
 	public String load(String path) {
 		System.out.println("Abrindo leitor");
-		InputStream fileStream = new FileInputStream(path);
-		System.out.println("Leitor aberto");
+		InputStream fileStream;
+		try{
+			fileStream = new FileInputStream(path);
+			System.out.println("Leitor aberto");
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Arquivo não encontrado");
+		}
+		
 
 		Reader fileReader = new InputStreamReader(fileStream, decoder);
 		BufferedReader reader = new BufferedReader(fileReader);
-
+		
+		
 		System.out.println("Lendo conteúdo");
-		String content = "";
-		String line = reader.readLine();
-		while (line != null) {
-			content += line;
-			line = reader.readLine();
+		String content;
+		try {
+		    content = readContent(reader);
+		    System.out.println("Conteúdo lido");
+		} catch (IOException e) {
+		    System.err.println("Erro ao ler conteúdo: " + e.getMessage());
+		    content = null;
 		}
-		content += "\n";
-		System.out.println("Conteúdo lido");
 
 		System.out.println("Fechando leitor");
-		reader.close();
-		System.out.println("Leitor fechado");
+		try {
+			reader.close();
+			System.out.println("Leitor fechado");
+		} catch(IOException e) {
+			System.err.println("Erro ao fechar leitor");
+		}
+		
 
 		return content;
 	}
 
-	public void save(String path, String content) {
+	public void save(String path, String content) { // onde vai escrever e o que vai escrever 
 		System.out.println("Abrindo escritor");
-		FileWriter writer = new FileWriter(path);
-		System.out.println("Escritor aberto");
+		FileWriter writer;
+		try{
+			writer = new FileWriter(path);
+			System.out.println("Escritor aberto");
+		} catch (IOException e) {
+			throw new IllegalArgumentException("Erro ao abrir escritor: " + e.getMessage());
+		}
+		
 
 		System.out.println("Escrevendo conteúdo");
-		writer.write(content);
-		System.out.println("Conteúdo escrito");
+		try {
+			writer.write(content);
+			System.out.println("Conteúdo escrito");
+		} catch(IOException e) {
+			System.err.println("Erro ao escrever conteúdo");
+		}
+		
 
 		System.out.println("Fechando escritor");
-		writer.close();
-		System.out.println("Escritor fechado");
+		try {
+			writer.close();
+			System.out.println("Escritor fechado");
+		} catch(IOException e) {
+			System.err.println("Erro ao fechar escritor");
+		}
+		
 	}
 }
--- 10/src/test/java/br/edu/insper/desagil/aula10/FileManagerTest.java
+++ 10/src/test/java/br/edu/insper/desagil/aula10/FileManagerTest.java
@@ -1,5 +1,10 @@
 package br.edu.insper.desagil.aula10;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,17 +18,32 @@ public class FileManagerTest {
 
 	@Test
 	public void load() {
+		String content = assertDoesNotThrow(() -> {
+			return f.load("entrada.txt");
+		});
+		assertEquals("hello world\n", content);
 	}
 
 	@Test
 	public void loadMissing() {
+		IllegalArgumentException  e = assertThrows(IllegalArgumentException .class, () -> {
+			f.load("missing.txt");
+		});
+		assertEquals("Arquivo não encontrado", e.getMessage());
 	}
 
 	@Test
 	public void loadInvalid() {
+		String content = assertDoesNotThrow(() -> {
+			return f.load("invalid.txt");
+		});
+		assertNull(content);
 	}
 
 	@Test
 	public void save() {
+		assertDoesNotThrow(() -> {
+		    f.save("hello world\n", "saida.txt");
+		});
 	}
 }
